15d04da71cab863f3815b37e6ded8afb042a452f
elFeliz <felix.kunze@t-online.de>
2024-12-21 11:30:52 +0100

    chore: bump dependencies
    - add ConfigProvider to root layout
        - resolves TypeError caused by [antd](https://github.com/ant-design/ant-design/issues/51339)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,16 @@ import type { Metadata } from "next";
 import StyledComponentsRegistry from "./AntdRegistry";
 import "./globals.css";
 import "./ui/icons.css";
+import { ConfigProvider } from "antd";
 
 export const metadata: Metadata = {
-  title: process.env.PROJECT_NAME ? process.env.PROJECT_NAME : "Crewstand Frontend",
+  title: process.env.PROJECT_NAME
+    ? process.env.PROJECT_NAME
+    : "Crewstand Frontend",
   description: "https://github.com/FelizCoder/crewstand.frontend",
-  applicationName: process.env.PROJECT_NAME ? process.env.PROJECT_NAME : "Crewstand Frontend",
+  applicationName: process.env.PROJECT_NAME
+    ? process.env.PROJECT_NAME
+    : "Crewstand Frontend",
   generator: "Next.js",
   publisher: "FelizCoder",
   authors: [{ name: "FelizCoder", url: "https://github.com/FelizCoder" }],
@@ -18,9 +23,13 @@ export const metadata: Metadata = {
 function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <html lang="en">
-      <body>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
-      </body>
+      <ConfigProvider wave={{ disabled: true }}>
+        {/* Workaround for TypeError: reactRender is not a function. 
+        might be resolved in future antd version. https://github.com/ant-design/ant-design/issues/51339*/}
+        <body>
+          <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        </body>
+      </ConfigProvider>
     </html>
   );
 }
