ea74f44a6bb7ce21618c0a7a6197a181e5b3f618
Felix Kunze <felix.kunze@t-online.de>
2025-02-26 14:54:41 +0100

    feat(automatic): Enhance Automatic Page with Solenoid Valves and Queue Mission Button
    * Added solenoid valves listing and control to Automatic Page
    * Introduced Queue Mission Button with partially implemented drawer (content TBD)
    * Updated apiCalls to include solenoid list retrieval
    * Extended icons import for additional symbols

--- app/automatic/page.tsx
+++ app/automatic/page.tsx
@@ -3,12 +3,39 @@
 import { useEffect, useState } from "react";
 import { FlowmeterWidgets } from "../sensors/ui/flowmeterWidgets";
 import SetpointInput from "./setpointInput";
+import { Button, Drawer, Space } from "antd";
+import { HandleAllButtons } from "../actuators/ui/handleAllButtons";
+import { ActuatorSwitch } from "../actuators/ui/actuatorSwitches";
+import { SolenoidValve } from "../api";
+import { getSolenoidsList } from "../actuators/apiCalls";
+import { QueueMissionButton } from "./queueMissionDrawer";
 
 export default function Page() {
   const [hostname, setHostname] = useState<string | undefined>(undefined);
+  let [solenoidValves, setSolenoidValves] = useState<
+    SolenoidValve[] | undefined
+  >(undefined);
+
+  const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
+
+  const showDrawer = () => {
+    setDrawerOpen(true);
+  };
+
+  const onDrawerClose = () => {
+    setDrawerOpen(false);
+  };
+
+  async function listSolenoids() {
+    console.debug("Fetching Actuators");
+    const solenoids = await getSolenoidsList();
+    setSolenoidValves(solenoids);
+    console.debug("Got Actuators: \n" + JSON.stringify(solenoids));
+  }
 
   useEffect(() => {
     setHostname(window.location.hostname);
+    listSolenoids();
   }, []);
 
   return (
@@ -21,6 +48,43 @@ export default function Page() {
           Flowmeters
         </h2>
         <FlowmeterWidgets />
+        {/* Solenoid Valves Section */}
+        <>
+          <div>
+            <Space wrap direction="horizontal" align="baseline" size={"large"}>
+              <h2>
+                <span className="material-symbols-outlined">valve</span>{" "}
+                Solenoid Valves
+              </h2>
+              <HandleAllButtons />
+            </Space>
+          </div>
+          {solenoidValves && solenoidValves.length > 0 && (
+            <Space size={"large"} wrap>
+              {solenoidValves.map((solenoid) => (
+                <ActuatorSwitch
+                  actuator={solenoid}
+                  websocketHostname={window.location.hostname}
+                  actuatorRoute={
+                    "/v1/actuators/solenoid/state/" + String(solenoid.id)
+                  }
+                  key={solenoid.type + String(solenoid.id)}
+                />
+              ))}
+            </Space>
+          )}
+        </>
+
+        {/* Setpoint Section */}
+        <div>
+          <Space wrap direction="horizontal" align="baseline" size={"large"}>
+            <h2>
+              <span className="material-symbols-outlined">play_arrow</span>{" "}
+              Current Setpoint
+            </h2>
+            <QueueMissionButton />
+          </Space>
+        </div>
 
         {hostname && (
           <SetpointInput
