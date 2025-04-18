a8572eb5fbe9c4e7f66ebd5b156140c6b5b2b5d9
Felix Kunze <felix.kunze@t-online.de>
2025-04-18 13:33:21 +0200

    feat: add MissionStats component to Crewstand Dashboard
    - Import MissionStats component from classification/UI
    - Integrate MissionStats with dynamic websocketHostname based on page hostname
    - Adjust iframe height to 250px for better layout balance
    
    The new MissionStats component is now integrated into the Crewstand Dashboard UI allowing real-time mission status updates. Decreased iframe size to ensure better utilization of screen real estate and to fit the new component without overcrowding.
    
    issue  #40

---
+++ app/classificatrion/ui/missionStats.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import { CompletedFlowControlMission } from "../../api";
+import useWebSocket from "../../hooks/useWebSocket";
+
+export interface MissionStatsProps {
+  websocketHostname: string;
+  missionRoute: string;
+}
+
+// Utility function to round time to 3 digits precision
+function roundTimestampTo3DigitsPrecision(timestamp: string): string {
+  const fractionStartIndex = timestamp.indexOf(".");
+  if (fractionStartIndex === -1) return timestamp;
+  const fractionPart = timestamp.substring(fractionStartIndex);
+  const roundedFraction = fractionPart.substring(
+    0,
+    Math.min(fractionPart.length, 4)
+  );
+  return timestamp.substring(0, fractionStartIndex) + roundedFraction;
+}
+
+export const MissionStats: React.FC<MissionStatsProps> = ({
+  websocketHostname,
+  missionRoute: sensorRoute,
+}) => {
+  const { data: mission, error } = useWebSocket<CompletedFlowControlMission>({
+    hostname: websocketHostname,
+    route: sensorRoute,
+  });
+  const [startTimestamp, setStartTimestamp] = useState<string | undefined>(
+    undefined
+  );
+  const [endTimestamp, setEndTimestamp] = useState<string | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    if (mission) {
+      // Round the start and end timestamps to 3 digit precision
+      const roundedStartTimestamp = roundTimestampTo3DigitsPrecision(
+        mission.start_ts
+      );
+      const roundedEndTimestamp = roundTimestampTo3DigitsPrecision(
+        mission.end_ts
+      );
+
+      setStartTimestamp(roundedStartTimestamp);
+      setEndTimestamp(roundedEndTimestamp);
+      console.debug(
+        `start: ${roundedStartTimestamp}, stop: ${roundedEndTimestamp}`
+      );
+    }
+  }, [mission]);
+
+  if (!mission)
+    return (
+      <div>No classification available. Waiting for an Event to complete.</div>
+    ); // Just an example for handling absence of mission
+
+  return (
+    <div>
+      {/* Displaying rounded timestamps */}
+      {startTimestamp && endTimestamp && (
+        <>
+          <div>
+            <iframe
+              src={`http://${websocketHostname}:3000/d/fej9lz2yz3sw0d/classification?folderUid=cej9lns9v9af4b&orgId=1&from=${startTimestamp}Z&to=${endTimestamp}Z&timezone=browser&viewPanel=panel-2&kiosk`}
+              height={250}
+              width="50%"
+            />
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { Space } from "antd";
+import { MissionStats } from "./classificatrion/ui/missionStats";
 
 export default function Page() {
   const [hostname, setHostname] = useState<string | undefined>(undefined);
@@ -15,9 +16,17 @@ export default function Page() {
       <h1>CREWSTAND DASHBOARD</h1>
       {hostname && (
         <div>
+          <MissionStats
+            missionRoute="/v1/missions/flow/completed"
+            websocketHostname={hostname}
+          />
+        </div>
+      )}
+      {hostname && (
+        <div>
           <iframe
             src={`http://${hostname}:3000/d/fej9lz2yz3sw0d/classification?folderUid=cej9lns9v9af4b&orgId=1&from=now-5m&to=now&timezone=browser&refresh=5s&viewPanel=panel-1&kiosk`}
-            height={750}
+            height={250}
             width="100%"
           />
         </div>
